f4a266c5219a723331d3f8e85253fde0c17ffc06
Domingo <domingo.benoit@alumnos.inf.utfsm.cl>
2025-06-10 23:10:41 -0400

    chore(multilabel): starting on multilabel support

--- mmice/task_loader.py
+++ mmice/task_loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datasets import Dataset
+from datasets import Dataset, Sequence, Value, Features
 
 # Task loader script in case someone wants to expand and add a new task
 # just create a loader here that returns a Hugginface Dataset object
@@ -15,14 +15,29 @@ def load_chilean_hate(data_files=None, column_names=['text', 'Odio']):
     data = data.shuffle(42)
     return data.train_test_split(train_size=.89)
 
-def load_42k_hcuch(data_files=None, column_names=['hallazgos', 'impresion', 'nodulos']):
-    kwargs = {"encoding" : "utf-8", "sep" : "|", "dtype": {'nodulos' : int}}
+def load_42k_hcuch(data_files=None,
+                   column_names=['hallazgos', 'impresion',
+                                 'condensacion', 'nodulos', 'quistes']):
+    kwargs = {"encoding" : "utf-8",
+              "sep" : "|",
+              "dtype":
+                  {'condensacion': int, 'nodulos': int, 'quistes': int}}
+
+    def combine_columns(example):
+        example["labels"] = [example["condensacion"], example["nodulos"], example["quistes"]]
+        return example
+
     df_list = [pd.read_csv(data_file, **kwargs) for data_file in data_files]
     df = pd.concat(df_list)[column_names]
     df['text'] = df['hallazgos'] # + " " +  df['impresion']
     
     data = Dataset.from_pandas(df).remove_columns(['hallazgos', 'impresion', '__index_level_0__'])
-    data = data.rename_column('nodulos', 'label')
+    data = data.map(combine_columns)
+    new_features = data.features.copy()
+    new_features["labels"] = Sequence(feature=Value("int8"), length=3)
+
+    data = data.cast(new_features)
+    data = data.remove_columns(['condensacion', 'nodulos', 'quistes'])
     data = data.shuffle(42)
     # For multilabel models we will make it focus only on the highest probability label
     return data.train_test_split(train_size=.75)
